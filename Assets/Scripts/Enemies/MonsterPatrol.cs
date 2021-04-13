84e252b966f519b4f4c2cf413c0eac4cd0f64846
Georghe Julca <petergeorghe.julca@enti.cat>
2021-04-13 13:46:07 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Assets/Scripts/Enemies/MonsterPatrol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPatrol : MonoBehaviour
+{
+    public float speed = 2f;
+    public Rigidbody2D rb;
+    public LayerMask groundLayers;
+
+    public Transform groundCheck;
+    bool isFacingRight = true;
+
+    RaycastHit2D hit;
+}
