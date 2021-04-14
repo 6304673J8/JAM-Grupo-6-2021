9fd9e4fbf180e38bb2b3fc9b097c67498a3f1581
Georghe Julca <petergeorghe.julca@enti.cat>
2021-04-14 11:24:42 +0200

    Enemy Walks W/ Hammer

--- Assets/Scripts/Hammer/Thrower.cs
+++ Assets/Scripts/Hammer/Thrower.cs
@@ -14,20 +14,25 @@ public class Thrower : MonoBehaviour
     public Animator animator;
     private int attackHammerID;
     private int noHammerID;
+    private int runNoHammerID;
+    private int jumpNoHammerID;
 
     bool attacked = false;
     private void Start()
     {
         attackHammerID = Animator.StringToHash("ThrownH");
         noHammerID = Animator.StringToHash("isBack");
-
+        runNoHammerID = Animator.StringToHash("isNoHammerMoving");
+        jumpNoHammerID = Animator.StringToHash("isNoHammerJumping");
     }
     // Update is called once per frame
     void Update()
     {
         attacked = false;
+        bool hasJumped = false;
 
         _travelledDistance += Time.deltaTime;
+        bool isMoving = false;
 
         if (Input.GetKeyDown("e") && GetComponent<PlayerController>().hammer)
         {
@@ -38,8 +43,23 @@ public class Thrower : MonoBehaviour
             throwerVisible.SetActive(false);
             GetComponent<PlayerController>().hammer = false;
             animator.SetBool(noHammerID, false);
-
         }
+        
+        if (Input.GetKey("space"))
+        {
+            hasJumped = true;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            isMoving = true;
+        }
+        else if (Input.GetKey("a"))
+        {
+            isMoving = true;
+        }
+        animator.SetBool(runNoHammerID, isMoving);
+        animator.SetBool(jumpNoHammerID, hasJumped);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
